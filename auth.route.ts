da806fb4154a94cf7a0dc18596f408baacf177f1
tejaschaudhari93 <tejaschaudhari93@gmail.com>
2023-04-07 20:37:08 +0530

    Add files via upload

---
+++ auth.route.ts
@@ -0,0 +1,26 @@
+
+
+import {Request, Response} from 'express';
+import {authenticate} from "./db-data";
+
+
+
+
+export function loginUser(req: Request, res: Response) {
+
+    console.log("User login attempt ..."+JSON.stringify(req.body));
+
+    const {email, password} = req.body;
+
+    const user = authenticate(email, password);
+
+    if (user) {
+        res.status(200).set('Cache-Control', 'public').json({id:user.id, email: user.email});
+    }
+    else {
+        res.sendStatus(403);
+    }
+
+}
+
+
